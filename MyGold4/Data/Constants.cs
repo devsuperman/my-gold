0b2476563a18707a9404e3c241a7522542d3d22f
Superman <hsbtiago@live.com>
2026-04-11 13:48:57 -0300

    estrutura de pastas

--- MyGold4/Data/Constants.cs
+++ MyGold4/Data/Constants.cs
@@ -1,7 +1,7 @@
 namespace MyGold4.Data;
 public static class Constants
 {
-    public const string DatabaseFilename = "AppSQLite.db3";
+    public const string DatabaseFilename = "AppSQLite2.db3";
     public static string DatabasePath => $"Data Source={Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename)}";
 
     public const SQLite.SQLiteOpenFlags Flags =
