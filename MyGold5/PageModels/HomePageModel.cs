a142f1d8ed3b0c708f0bacea8e125d112195a43c
Superman <hsbtiago@live.com>
2026-04-14 17:44:19 -0300

    home page top

--- MyGold5/PageModels/HomePageModel.cs
+++ MyGold5/PageModels/HomePageModel.cs
@@ -3,15 +3,42 @@ using CommunityToolkit.Mvvm.Input;
 using MyGold5.Models;
 
 namespace MyGold5.PageModels;
-public partial class HomePageModel(SpentRepository repository) : ObservableObject
+
+public partial class HomePageModel(SpentRepository repository, CategoryRepository categoryRepository) : ObservableObject
 {
+    [ObservableProperty]
+    List<Category> _categories = [];
 
     [ObservableProperty]
-    private List<Spent> _list = [];
+    List<TotalByCategory> _totals = [];
 
     [RelayCommand]
     private async Task Appearing()
     {
-        List = await repository.ListAsync(DateTime.Today, DateTime.Today, 0);
+        Categories = await categoryRepository.ListAsync();
+        var expenses = await repository.ListAsync(DateTime.Today, DateTime.Today, 0);
+
+        var listTotals = expenses
+            .GroupBy(g => g.CategoryId)
+            .Select(g => new TotalByCategory
+            {
+                CategoryId = g.Key,
+                Total = g.Sum(s => s.Value)
+            })
+            .ToList();
+
+        foreach (var total in listTotals)
+        {
+            var category = Categories.FirstOrDefault(f => f.ID == total.CategoryId);
+            total.Category = category?.Name;
+        }
+
+        Totals = new(listTotals);
+    }
+
+    [RelayCommand]
+    public async Task AddAsync()
+    {
+        await Shell.Current.GoToAsync("spent");
     }
 }
